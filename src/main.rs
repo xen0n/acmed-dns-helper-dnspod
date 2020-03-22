972d942b33df1813c50a118a88bc28d53813c36c
WANG Xuerui <git@xen0n.name>
2020-03-22 17:09:31 +0800

    feat: get dnspod credentials from env

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,36 @@ struct Args {
     clean: bool,
 }
 
+#[derive(Debug)]
+struct DnspodCredentials {
+    id: u64,
+    token: String,
+}
+
+impl DnspodCredentials {
+    fn try_from_env() -> Result<Self, Box<dyn std::error::Error + Send + Sync>> {
+        use std::env;
+
+        Ok(Self {
+            id: env::var("DNSPOD_ID")?.parse()?,
+            token: env::var("DNSPOD_TOKEN")?,
+        })
+    }
+
+    fn to_login_token(&self) -> String {
+        format!("{},{}", self.id, self.token)
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     env_logger::init();
 
     let args = Args::from_args();
+    let dnspod_creds = DnspodCredentials::try_from_env()?;
 
     info!("args = {:?}", args);
+    info!("dnspod creds = {:?}", dnspod_creds);
+
     Ok(())
 }
