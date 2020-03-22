b58a0a558dc5620e278d8fbc0e808ac7ad8a4b6b
WANG Xuerui <git@xen0n.name>
2020-03-22 17:03:02 +0800

    feat: add argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
 use log::info;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "acmed-dns-helper-dnspod")]
+struct Args {
+    #[structopt(long)]
+    domain: String,
+    #[structopt(long)]
+    proof: String,
+    #[structopt(long)]
+    clean: bool,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     env_logger::init();
 
-    info!("Hello, world!");
+    let args = Args::from_args();
+
+    info!("args = {:?}", args);
     Ok(())
 }
