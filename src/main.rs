131c3b1f0164daaee2eaa4ee6708b768a4c61a75
WANG Xuerui <git@xen0n.name>
2020-03-22 17:54:41 +0800

    feat: add client

--- src/main.rs
+++ src/main.rs
@@ -33,15 +33,68 @@ impl DnspodCredentials {
     }
 }
 
+#[derive(Debug)]
+struct DnspodUserAgent {
+    contact_email: String,
+}
+
+impl DnspodUserAgent {
+    fn try_from_env() -> Result<Self, Box<dyn std::error::Error + Send + Sync>> {
+        Ok(Self {
+            contact_email: std::env::var("DNSPOD_CONTACT_EMAIL")?,
+        })
+    }
+
+    fn to_ua_string(&self) -> String {
+        // XXX: get version from Cargo.toml
+        format!("acmed-dns-helper-dnspod/0.1.0 ({})", self.contact_email)
+    }
+}
+
+impl From<DnspodUserAgent> for reqwest::header::HeaderValue {
+    fn from(a: DnspodUserAgent) -> Self {
+        Self::from_str(&a.to_ua_string()).unwrap()
+    }
+}
+
+#[derive(Debug)]
+struct DnspodClient<'a> {
+    login_token: String,
+    client: &'a reqwest::Client,
+    api_host: url::Url,
+}
+
+const DEFAULT_API_HOST: &str = "https://dnsapi.cn";
+
+impl<'a> DnspodClient<'a> {
+    fn new(client: &'a reqwest::Client, creds: &DnspodCredentials) -> Self {
+        let login_token = creds.to_login_token();
+
+        Self {
+            login_token: login_token,
+            client: client,
+            api_host: DEFAULT_API_HOST.parse().unwrap(),
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     env_logger::init();
 
     let args = Args::from_args();
     let dnspod_creds = DnspodCredentials::try_from_env()?;
+    let dnspod_ua = DnspodUserAgent::try_from_env()?;
 
     info!("args = {:?}", args);
     info!("dnspod creds = {:?}", dnspod_creds);
+    info!("dnspod_ua = {:?}", dnspod_ua);
+
+    let http = reqwest::ClientBuilder::new()
+        .user_agent(dnspod_ua)
+        .build()?;
+
+    let _dnspod = DnspodClient::new(&http, &dnspod_creds);
 
     Ok(())
 }
